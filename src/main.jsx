5ec7a49c854028ef5ec3c3ffb2f42adb5ea115c8
HombreOmbre <marcin.obrebowski@outlook.com>
2024-09-30 19:38:18 +0200

    Add contexts and router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { routes } from "../routes.jsx";
+import { routes } from "./routes/routes.jsx";
+import "./styles/globals.scss"
+import { MainProvider } from "./context/MainProvider.jsx";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+      <MainProvider>
+        <RouterProvider router={router} />
+      </MainProvider>
   </StrictMode>,
 )
