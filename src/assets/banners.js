15db22fea861341abf889ae5e1ade09ebd0f322b
HombreOmbre <marcin.obrebowski@outlook.com>
2024-09-30 20:03:39 +0200

    Add js file with array of banners href

---
+++ src/assets/banners.js
@@ -0,0 +1,27 @@
+import banner1 from "./banner_id1.png";
+import banner2 from "./banner_id2.png";
+import banner3 from "./banner_id3.png";
+import banner4 from "./banner_id4.png";
+
+export const bannersData = [
+    {
+        id: '1',
+        img: banner1,
+        alt: 'Fjallraven Foldsack Backpack'
+    },
+    {
+        id: '2',
+        img: banner2,
+        alt: 'Casual Premium Slim Fit T-shirt'
+    },
+    {
+        id: '3',
+        img: banner3,
+        alt: 'Mens Cotton Jacket'
+    },
+    {
+        id: '4',
+        img: banner4,
+        alt: 'Mens Casual Slim Fit'
+    }
+];
