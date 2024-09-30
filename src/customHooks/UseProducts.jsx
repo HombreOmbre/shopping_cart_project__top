aa3315cf1019506881ced3cd43282104f4c55f9e
HombreOmbre <marcin.obrebowski@outlook.com>
2024-09-30 19:30:41 +0200

    Add custom hook for fetching products

---
+++ src/customHooks/UseProducts.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+const fetchProducts = async (setProducts, setError, setLoading) => {
+    try {
+        const response = await fetch('https://fakestoreapi.com/products?limit=15');
+
+        if (!response.ok) {
+            throw new Error(response.error);
+        }
+
+        const productsData = await response.json();
+
+        setProducts(productsData);
+        setError(false);
+    } catch (error) {
+        setError(error);
+        setProducts([]);
+    } finally {
+        setLoading(false);
+    }
+}
+
+export const UseProducts = () => {
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(fetchProducts.bind(null, setProducts ,setError, setLoading), 0)
+    }, []);
+
+    return { products, error, loading };
+}
