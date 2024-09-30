9c4868210dfead7d16cb5a1d45dbda40b31a86e5
HombreOmbre <marcin.obrebowski@outlook.com>
2024-09-30 19:40:12 +0200

    Add Header component with use memo to calculate number of products, module styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { Link, NavLink } from "react-router-dom";
+import styles from "../styles/Header.module.scss";
+import { useContext, useMemo } from "react";
+import { ShoppingCartContext } from "../context/ShoppingCartContext.jsx";
+
+export const Header = () => {
+    const shoppingCart = useContext(ShoppingCartContext)
+    const productsAmount = useMemo(() => {
+        return shoppingCart.shoppingCart.reduce((acc, item) => acc += item.getProductAmount(), 0)
+    },[shoppingCart]);
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.container}>
+                <div className={styles.leftBox}>
+                    <Link className={styles.link} to="/">
+                        Shopperooo
+                    </Link>
+                </div>
+                <div className={styles.rightBox}>
+                    <nav className={styles.nav}>
+                        <NavLink className={styles.link} to="/">Home Page</NavLink>
+                        <NavLink className={styles.link} to="/">Products</NavLink>
+                        <NavLink className={styles.link} to="/">
+                            Checkout ({productsAmount})
+                        </NavLink>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+}
