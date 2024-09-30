9133344c49902358c68d00e7163b10b49bd9855e
HombreOmbre <marcin.obrebowski@outlook.com>
2024-09-30 20:06:30 +0200

    Add file with all titles and links for map of site

---
+++ src/assets/mapSites.js
@@ -0,0 +1,37 @@
+export const mapSites = [
+    {
+        title: "About Us",
+        links: [
+            "Company History",
+            "Meet the Team",
+            "Employee Handbook",
+            "Careers"
+        ],
+    },
+    {
+        title: "Our Services",
+        links: [
+            "Web Development",
+            "Web Design",
+            "Marketing",
+            "Google Ads"
+        ],
+    },
+    {
+        title: "Resources",
+        links: [
+            "Online Guides",
+            "Conference Notes",
+            "Downloads",
+            "Upcoming Events"
+        ],
+    },
+    {
+        title: "Helpful Links",
+        links: [
+            "FAQs",
+            "Support",
+            "Forum"
+        ],
+    },
+]
