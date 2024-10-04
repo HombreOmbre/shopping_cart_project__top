7f307adbd46c2730162941dd041965c0157bd3b2
HombreOmbre <marcin.obrebowski@outlook.com>
2024-10-04 20:09:18 +0200

    Add routes for links in banner to showing productCart

--- src/components/CarouselBanner.jsx
+++ src/components/CarouselBanner.jsx
@@ -14,11 +14,19 @@ export const CarouselBanner = () => {
 
     return (
         <div className={styles.mainContainer}>
-            <Carousel activeIndex={index} onSelect={handleSelect} fade controls={false} indicators={false}  interval={1500} className={styles.container}>
+            <Carousel
+                activeIndex={index}
+                onSelect={handleSelect}
+                controls={true}
+                indicators={false}
+                interval={1000}
+                className={styles.container}
+                fade
+            >
                 {
                     bannersData.map(item => (
                         <Carousel.Item key={item.id}>
-                            <Link to="/">
+                            <Link to={"/products/" + item.id}>
                                 <img src={item.img} alt={item.alt} className={styles.img} />
                             </Link>
                         </Carousel.Item>
